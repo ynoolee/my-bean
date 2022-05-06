1b0916a7a262fd42290eea1d364731c75bb0bc91
ynoolee <heerarea129@gmail.com>
2022-05-06 23:27:32 +0900

    Init project

---
+++ src/main/java/com/example/mybean/MyBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mybean/MyBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
